32864b8c76d2cd7efc19712e0974c9c82aa8ad0c
Eric Kidd <git@randomhacks.net>
2016-12-22 15:46:08 -0500

    Actually clean the records and write them out
    This drops our performance from 225 MB/s to 67 MB/s on my machine
    because we can't really use the zero-copy APIs for this application,
    because we need to see a whole record before deciding whether to output
    it.

--- src/errors.rs
+++ src/errors.rs
@@ -13,4 +13,31 @@ error_chain! {
         Csv(csv::Error);
         Io(io::Error);
     }
+
+    errors {
+        TooManyBadRows(bad: u64, total: u64) {
+            description("a large portion of your rows were bad")
+            display("a large portion of your rows ({} of {}) were bad", bad, total)
+        }
+    }
+}
+
+impl Error {
+    /// Should we show a backtrace for this particular error?
+    pub fn should_show_backtrace(&self) -> bool {
+        match *self.kind() {
+            ErrorKind::TooManyBadRows(_, _) => false,
+            _ => true,
+        }
+    }
+
+    /// What exit code should we return when the process exits?
+    pub fn to_exit_code(&self) -> i32 {
+        match *self.kind() {
+            // This is only arguably an error, so return a special code for
+            // people who want to try to ignore it.
+            ErrorKind::TooManyBadRows(_, _) => 2,
+            _ => 1,
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -33,14 +33,19 @@ Usage: scrubcsv [options] [<input>]
        scrubcsv --version
 
 Read a CSV file, normalize the "good" lines, and print them to standard
-output.  Discard any lines with the wrong number of columns
+output.  Discard any lines with the wrong number of columns.
 
 Options:
     --help                Show this help message
     --version             Print the version of this program
-    -v, --verbose
+    -q, --quiet           Do not print performance information
     -d, --delimiter CHAR  Character used to separate fields in a row
                           (must be a single ASCII byte) [default: ","]
+
+Exit code:
+  0 on success
+  1 on error
+  2 if more than 10% of rows were bad
 "#;
 
 /// Our command-line arguments.
@@ -48,6 +53,7 @@ Options:
 struct Args {
     arg_input: Option<String>,
     flag_delimiter: String,
+    flag_quiet: bool,
 }
 
 /// This is a helper function called by our `main` function.  Unlike
@@ -88,7 +94,7 @@ fn run(args: &Args) -> Result<()> {
     // ...or opening up `/dev/stdout`, but the solution below is within
     // measurement error in Rust 1.13.0, so no need to get cute.
     let stdout = io::stdout();
-    let mut output = io::BufWriter::new(stdout.lock());
+    let output = io::BufWriter::new(stdout.lock());
 
     // Fetch our input from either standard input or a file.  The only
     // tricky detail here is that we use a `Box<Read>` to represent "some
@@ -104,30 +110,75 @@ fn run(args: &Args) -> Result<()> {
     };
     let input = io::BufReader::new(unbuffered_input);
 
+    // Create our CSV reader.  Note that we do not allow you to set the
+    // quoting character; if you want to do that, please think through the
+    // implications carefully.
+    let mut rdr = csv::Reader::from_reader(input)
+        // Treat headers (if any) as any other record.
+        .has_headers(false)
+        // Allow records with the wrong number of columns.
+        .flexible(true)
+        // Configure our delimiter.
+        .delimiter(delimiter);
+
+    // Create our CSV writer.  Note that we _don't_ allow variable numbers
+    // of columns, non-standard delimiters, or other nonsense: We want our
+    // output to be highly normalized.
+    let mut wtr = csv::Writer::from_writer(output);
+
     // Use the low-level CSV parser API, which doesn't allocate memory.
     let mut rows: u64 = 0;
-    let mut rdr = csv::Reader::from_reader(input)
-        .flexible(true)
-        .delimiter(delimiter);
-    while !rdr.done() {
-        loop {
-            match rdr.next_bytes() {
-                csv::NextField::EndOfCsv => break,
-                csv::NextField::EndOfRecord => { rows += 1; break; },
-                csv::NextField::Error(err) => { return Err(From::from(err)); }
-                csv::NextField::Data(_) => {}
+    let mut bad_rows: u64 = 0;
+
+    // Iterate over all the rows, checking to make sure they look
+    // reasonable.
+    //
+    // If we use the lowest-level, zero-copy API for `csv`, we can process
+    // about 225 MB/s.  But it turns out we can't do that, because we need to
+    // have a copy of all the row's fields before deciding whether or not
+    // to write it out.
+    let mut columns_expected = None;
+    for record in rdr.byte_records() {
+        let record = record?;
+
+        // Keep track of how many columns we expected.
+        let is_valid = if let Some(cols) = columns_expected {
+            if record.len() != cols {
+                bad_rows += 1;
+                false
+            } else {
+                true
             }
+        } else {
+            columns_expected = Some(record.len());
+            true
+        };
+
+        // If this is good row, output it.
+        if is_valid {
+            wtr.write(record.into_iter())?;
         }
+
+        rows += 1;
     }
 
     // Print out some information about our run.
-    let ellapsed = time::precise_time_s() - start_time;
-    let bytes_per_second = (rdr.byte_offset() as f64 / ellapsed) as i64;
-    writeln!(io::stderr(),
-             "{} rows in {:.2} seconds, {}/sec",
-             rows,
-             ellapsed,
-             bytes_per_second.file_size(file_size_opts::BINARY)?)?;
+    if !args.flag_quiet {
+        let ellapsed = time::precise_time_s() - start_time;
+        let bytes_per_second = (rdr.byte_offset() as f64 / ellapsed) as i64;
+        writeln!(io::stderr(),
+                 "{} rows ({} bad) in {:.2} seconds, {}/sec",
+                 rows,
+                 bad_rows,
+                 ellapsed,
+                 bytes_per_second.file_size(file_size_opts::BINARY)?)?;
+    }
+
+    // If more than 10% of rows are bad, assume something has gone horribly
+    // wrong.
+    if bad_rows * 10 > rows {
+        return Err(ErrorKind::TooManyBadRows(bad_rows, rows).into());
+    }
 
     Ok(())
 }
@@ -153,9 +204,14 @@ fn main() {
             write!(&mut stderr, ": {}", e).unwrap();
         }
         writeln!(&mut stderr, "").unwrap();
-        if let Some(backtrace) = err.backtrace() {
-            writeln!(&mut stderr, "Backtrace:\n{:?}", backtrace).unwrap();
+
+        // Decide whether to show a backtrace (if we have one at all);
+        if err.should_show_backtrace() {
+            if let Some(backtrace) = err.backtrace() {
+                writeln!(&mut stderr, "Backtrace:\n{:?}", backtrace).unwrap();
+            }
         }
-        process::exit(1);
+
+        process::exit(err.to_exit_code());
     }
 }
